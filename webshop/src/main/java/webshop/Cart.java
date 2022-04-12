8041caef10822a278f1ffe3c1cc4a4383d0f5172
JaniVarga86HUN <jani.varga86@gmail.com>
2022-04-12 18:29:11 +0200

    Item lista, és add methódus.

--- webshop/src/main/java/webshop/Cart.java
+++ webshop/src/main/java/webshop/Cart.java
@@ -1,4 +1,13 @@
 package webshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
 }
