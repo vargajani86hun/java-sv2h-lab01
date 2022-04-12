a92bd4ba5d1f758d5417c9c9baf44470514141c7
JaniVarga86HUN <jani.varga86@gmail.com>
2022-04-12 18:28:42 +0200

    Item osztály első funkciói.

--- webshop/src/main/java/webshop/Item.java
+++ webshop/src/main/java/webshop/Item.java
@@ -1,4 +1,23 @@
 package webshop;
 
 public class Item {
+    private Product product;
+    private int amount;
+
+    public Item(Product product, int amount) {
+        this.product = product;
+        this.amount = amount;
+    }
+
+    public void modifyAmount(int value) {
+        amount += value;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
