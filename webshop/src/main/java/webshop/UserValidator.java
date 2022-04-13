32cf2fa752496bee15246a7f2dbdca50cdc6c6ca
JaniVarga86HUN <jani.varga86@gmail.com>
2022-04-13 20:46:54 +0200

    UserValidator osztály elkezdve.

---
+++ webshop/src/main/java/webshop/UserValidator.java
@@ -0,0 +1,55 @@
+package webshop;
+
+public class UserValidator {
+
+    private String userName;
+    private String password;
+    private String email;
+    private UserDao userDao;
+
+    public UserValidator(String userName, String password, String email, UserDao userDao) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.userDao = userDao;
+    }
+
+    public void validateUserName() {
+        if (userName == null || userName.isEmpty()) {
+            throw new IllegalArgumentException("User name should not be empty!");
+        }
+
+        if (userDao.findUserByName(userName).isPresent()) {
+            throw new IllegalArgumentException("Name is already taken!");
+        }
+    }
+
+    public void validatePassword() {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password should not be empty!");
+        }
+
+        if (password.toLowerCase().equals(password) || password.toUpperCase().equals(password)) {
+            throw new IllegalArgumentException("Password should contain lowercase and uppercase characters too!");
+        }
+    }
+
+    public void validateEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("E-mail address should not be empty!");
+        }
+
+        if (email.length() < 3) {
+            throw new IllegalArgumentException("It's too short to be valid e-mail address");
+        }
+
+        if (email.indexOf('@') < 1) {
+            throw new IllegalArgumentException("E-mail address should contain '@' character!");
+        }
+
+        if (email.lastIndexOf('.') < email.indexOf('@')) {
+            throw new IllegalArgumentException("E-mail address should contain a domain at its end.");
+        }
+    }
+
+}
