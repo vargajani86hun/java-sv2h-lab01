320e80b4a3dabd0759b29a03ea66e88f9fb25980
JaniVarga86HUN <jani.varga86@gmail.com>
2022-04-14 10:12:30 +0200

    ProductDao-ban az insertProductban visszatér a generált id-val.

--- webshop/src/main/java/webshop/ProductDao.java
+++ webshop/src/main/java/webshop/ProductDao.java
@@ -2,7 +2,11 @@ package webshop;
 
 import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 public class ProductDao {
@@ -13,9 +17,17 @@ public class ProductDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void insertProduct(Product product) {//language=sql
-        jdbcTemplate.update("insert into products (product_name, price) values (?, ?)",
-                product.getName(), product.getPrice());
+    public long insertProduct(Product product) {//language=sql
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+                    PreparedStatement ps = con.prepareStatement("INSERT INTO products (product_name, price) VALUES (?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, product.getName());
+                    ps.setInt(2, product.getPrice());
+                    return ps;
+                },
+                keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
     public List<Product> getProducts() {
