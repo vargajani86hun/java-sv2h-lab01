7b78480fdc27174127de861557a828508d9e8129
codriv <combodriver4@yahoo.at>
2022-04-14 09:08:02 +0200

    Item add: sumPrice attribute, setPrice() method, getSumPrice() method.

--- webshop/src/main/java/webshop/Item.java
+++ webshop/src/main/java/webshop/Item.java
@@ -3,14 +3,21 @@ package webshop;
 public class Item {
     private Product product;
     private int amount;
+    private int sumPrice;
 
     public Item(Product product, int amount) {
         this.product = product;
         this.amount = amount;
+        setPrice();
+    }
+
+    private void setPrice() {
+        sumPrice = product.getPrice() * amount;
     }
 
     public void modifyAmount(int value) {
         amount += value;
+        setPrice();
     }
 
     public Product getProduct() {
@@ -19,5 +26,9 @@ public class Item {
 
     public int getAmount() {
         return amount;
+    }
+
+    public int getSumPrice() {
+        return sumPrice;
     }
 }
