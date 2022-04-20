b6414a2158c50f9e76fbe178c431d33b527f6412
JaniVarga86HUN <jani.varga86@gmail.com>
2022-04-20 23:39:47 +0200

    Equals() and hashCode() are added.

--- webshop/src/main/java/webshop/Product.java
+++ webshop/src/main/java/webshop/Product.java
@@ -1,5 +1,7 @@
 package webshop;
 
+import java.util.Objects;
+
 public class Product {
 
     private long id;
@@ -27,5 +29,17 @@ public class Product {
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return id == product.id && price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
     }
 }
